865355478bbc285de7fd79dc1eef6187c48b4739
Matheus <mthsbessac@gmail.com>
2025-01-23 21:54:04 -0300

    adding event description

--- backend/src/services/event.service.go
+++ backend/src/services/event.service.go
@@ -7,7 +7,8 @@ import (
 )
 
 type EventService interface {
-	CreateEvent(ctx context.Context, name string, ownerId uint32, orgId string) (models.Event, error)
+	CreateEvent(ctx context.Context, name string, ownerId uint32, orgId string, description string) (models.Event, error)
 	GetEvent(ctx context.Context, eventId string) (models.Event, error)
 	SetCover(ctx context.Context, id string, url string) error
+	//GetAllEvents(ctx context.Context, limit int) ([]models.Event, error)
 }
